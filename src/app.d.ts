dbe3357263e3a6a0b1d4112e7ede9251126cbe11
Evangelos Paterakis <evan@geopjr.dev>
2022-08-04 04:27:33 +0300

    chore: keep up with kit

---
+++ src/app.d.ts
@@ -0,0 +1,8 @@
+declare namespace App {
+	// interface Locals {}
+	// interface Platform {}
+	// interface PrivateEnv {}
+	// interface PublicEnv {}
+	// interface Session {}
+	// interface Stuff {}
+}
